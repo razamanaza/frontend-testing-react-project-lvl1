7902221348cb0f566d220555946642c5e5921813
Aleksandr Gordeev <razamanaza@yandex.ru>
2021-05-18 21:25:44 +1200

    More assets tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -66,6 +66,8 @@ describe('pageLoader', () => {
   });
   it('download site.com', async () => {
     const html = readFile(getFixturePath('site-com-blog-about.html'));
+    const filesDir = path.join(tempdir, 'site-com-blog-about_files');
+    const expectedAbout = path.join(__dirname, '..', '__fixtures__', 'expected', 'site-com-blog-about_files', 'site-com-blog-about.html');
     nock('https://site.com')
       .get('/blog/about')
       .reply(200, html, {
@@ -74,19 +76,18 @@ describe('pageLoader', () => {
       .get('/blog/about/assets/styles.css')
       .reply(200, 'File content')
       .get('/blog/about')
-      .reply(200, 'File content')
+      .replyWithFile(200, expectedAbout)
       .get('/photos/me.jpg')
       .reply(200, 'File content')
       .get('/assets/scripts.js')
       .reply(200, 'File content');
-    const filesDir = path.join(tempdir, 'site-com-blog-about_files');
     await pageLoader('https://site.com/blog/about', tempdir);
     expect(fileExists(path.join(filesDir, 'site-com-assets-scripts.js'))).toBe(true);
     let content = readFile(path.join(filesDir, 'site-com-assets-scripts.js'));
     expect(content).toEqual('File content');
     expect(fileExists(path.join(filesDir, 'site-com-blog-about.html'))).toBe(true);
     content = readFile(path.join(filesDir, 'site-com-blog-about.html'));
-    expect(content).toEqual('File content');
+    expect(content).toEqual(readFile(expectedAbout));
     expect(fileExists(path.join(filesDir, 'site-com-blog-about-assets-styles.css'))).toBe(true);
     content = readFile(path.join(filesDir, 'site-com-blog-about-assets-styles.css'));
     expect(content).toEqual('File content');
@@ -96,40 +97,6 @@ describe('pageLoader', () => {
     expect(fileExists(path.join(tempdir, 'site-com-blog-about.html'))).toBe(true);
     const expected = readFile(getFixturePath('expected/site-com-blog-about.html'));
     const replaced = readFile(path.join(tempdir, 'site-com-blog-about.html'));
-    expect(replaced).toEqual(expected);
-  });
-  it('download localhost', async () => {
-    const html = readFile(getFixturePath('localhost-blog-about.html'));
-    nock('http://localhost')
-      .get('/blog/about')
-      .reply(200, html, {
-        'Content-Type': 'text/html',
-      })
-      .get('/blog/about/assets/styles.css')
-      .reply(200, 'File content')
-      .get('/blog/about')
-      .reply(200, 'File content')
-      .get('/photos/me.jpg')
-      .reply(200, 'File content')
-      .get('/assets/scripts.js')
-      .reply(200, 'File content');
-    const filesDir = path.join(tempdir, 'localhost-blog-about_files');
-    await pageLoader('http://localhost/blog/about', tempdir);
-    expect(fileExists(path.join(filesDir, 'localhost-assets-scripts.js'))).toBe(true);
-    let content = readFile(path.join(filesDir, 'localhost-assets-scripts.js'));
-    expect(content).toEqual('File content');
-    expect(fileExists(path.join(filesDir, 'localhost-blog-about.html'))).toBe(true);
-    content = readFile(path.join(filesDir, 'localhost-blog-about.html'));
-    expect(content).toEqual('File content');
-    expect(fileExists(path.join(filesDir, 'localhost-blog-about-assets-styles.css'))).toBe(true);
-    content = readFile(path.join(filesDir, 'localhost-blog-about-assets-styles.css'));
-    expect(content).toEqual('File content');
-    expect(fileExists(path.join(filesDir, 'localhost-photos-me.jpg'))).toBe(true);
-    content = readFile(path.join(filesDir, 'localhost-photos-me.jpg'));
-    expect(content).toEqual('File content');
-    const expected = readFile(getFixturePath('expected/localhost-blog-about.html'));
-    const replaced = readFile(path.join(tempdir, 'localhost-blog-about.html'));
-    dbg(replaced);
     expect(replaced).toEqual(expected);
   });
 });
