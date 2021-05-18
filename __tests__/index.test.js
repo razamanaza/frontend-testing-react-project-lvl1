e43ddc776bbacf98810ec4373a0c9b20414ab867
Aleksandr Gordeev <razamanaza@yandex.ru>
2021-05-18 19:13:56 +1200

    More tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -84,10 +84,42 @@ describe('pageLoader', () => {
     expect(fileExists(path.join(tempdir, 'site-com-blog-about.html'))).toBe(true);
     expect(fileExists(path.join(filesDir, 'site-com-blog-about.html'))).toBe(true);
     expect(fileExists(path.join(filesDir, 'site-com-assets-scripts.js'))).toBe(true);
+    let content = readFile(path.join(filesDir, 'site-com-assets-scripts.js'));
+    expect(content).toEqual('File content');
     expect(fileExists(path.join(filesDir, 'site-com-blog-about-assets-styles.css'))).toBe(true);
+    content = readFile(path.join(filesDir, 'site-com-blog-about-assets-styles.css'));
+    expect(content).toEqual('File content');
     expect(fileExists(path.join(filesDir, 'site-com-photos-me.jpg'))).toBe(true);
+    content = readFile(path.join(filesDir, 'site-com-photos-me.jpg'));
+    expect(content).toEqual('File content');
     const expected = readFile(getFixturePath('expected/site-com-blog-about.html'));
     const replaced = readFile(path.join(tempdir, 'site-com-blog-about.html'));
+    expect(replaced).toEqual(expected);
+  });
+  it('download localhost', async () => {
+    const html = readFile(getFixturePath('localhost-blog-about.html'));
+    nock('http://localhost')
+      .get('/blog/about')
+      .reply(200, html, {
+        'Content-Type': 'text/html',
+      })
+      .get('/blog/about/assets/styles.css')
+      .reply(200, 'File content')
+      .get('/blog/about')
+      .reply(200, 'File content')
+      .get('/photos/me.jpg')
+      .reply(200, 'File content')
+      .get('/assets/scripts.js')
+      .reply(200, 'File content');
+    const filesDir = path.join(tempdir, 'localhost-blog-about_files');
+    await pageLoader('http://localhost/blog/about', tempdir);
+    expect(fileExists(path.join(tempdir, 'localhost-blog-about.html'))).toBe(true);
+    expect(fileExists(path.join(filesDir, 'localhost-blog-about.html'))).toBe(true);
+    expect(fileExists(path.join(filesDir, 'localhost-assets-scripts.js'))).toBe(true);
+    expect(fileExists(path.join(filesDir, 'localhost-blog-about-assets-styles.css'))).toBe(true);
+    expect(fileExists(path.join(filesDir, 'localhost-photos-me.jpg'))).toBe(true);
+    const expected = readFile(getFixturePath('expected/localhost-blog-about.html'));
+    const replaced = readFile(path.join(tempdir, 'localhost-blog-about.html'));
     expect(replaced).toEqual(expected);
   });
 });
