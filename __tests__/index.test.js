ee6f7f442619aa1a4388a492bb0efe271e281a2f
Aleksandr Gordeev <razamanaza@yandex.ru>
2021-05-23 15:57:29 +1200

    Fake content tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -21,95 +21,46 @@ describe('pageLoader', () => {
   });
   beforeEach(() => {
     tempdir = fs.mkdtempSync(path.join(os.tmpdir(), 'page-loader-'));
-    const outputFilesDir = path.join(tempdir, 'site-com-blog-about_files');
-    const expectedFilesDir = path.join(__dirname, '..', '__fixtures__', 'expected', 'site-com-blog-about_files');
-    data = {
-      html: {
-        input: getFixturePath('site-com-blog-about.html'),
-        output: path.join(tempdir, 'site-com-blog-about.html'),
-        expected: path.join(__dirname, '..', '__fixtures__', 'expected', 'site-com-blog-about.html'),
-      },
-      htmlAsset: {
-        input: path.join(expectedFilesDir, 'site-com-blog-about.html'),
-        output: path.join(outputFilesDir, 'site-com-blog-about.html'),
-        expected: path.join(expectedFilesDir, 'site-com-blog-about.html'),
-      },
-      img: {
-        input: path.join(expectedFilesDir, 'site-com-photos-me.jpg'),
-        output: path.join(outputFilesDir, 'site-com-photos-me.jpg'),
-        expected: path.join(expectedFilesDir, 'site-com-photos-me.jpg'),
-      },
-      js: {
-        input: path.join(expectedFilesDir, 'site-com-assets-scripts.js'),
-        output: path.join(outputFilesDir, 'site-com-assets-scripts.js'),
-        expected: path.join(expectedFilesDir, 'site-com-assets-scripts.js'),
-      },
-      css: {
-        input: path.join(expectedFilesDir, 'site-com-blog-about-assets-styles.css'),
-        output: path.join(outputFilesDir, 'site-com-blog-about-assets-styles.css'),
-        expected: path.join(expectedFilesDir, 'site-com-blog-about-assets-styles.css'),
-      },
-    };
-    nock('https://site.com')
-      .get('/blog/about')
-      .reply(200, readFile(data.html.input), {
-        'Content-Type': 'text/html',
-      })
-      .get('/blog/about/assets/styles.css')
-      .reply(200, readFile(data.css.input), {
-        'Content-Type': 'text/css',
-      })
-      .get('/blog/about')
-      .reply(200, readFile(data.htmlAsset.input), {
-        'Content-Type': 'text/html',
-      })
-      .get('/photos/me.jpg')
-      .reply(200, readFile(data.img.input), {
-        'Content-Type': 'image/jpeg',
-      })
-      .get('/assets/scripts.js')
-      .reply(200, readFile(data.js.input), {
-        'Content-Type': 'text/javascript',
-      });
+    nock('https://site.com').get('/blog/about').reply(200, readFile(getFixturePath('site-com-blog-about.html')))
+      .get('/blog/about/assets/styles.css').reply(200, 'css')
+      .get('/blog/about').reply(200, 'html')
+      .get('/photos/me.jpg').reply(200, 'img')
+      .get('/assets/scripts.js').reply(200, 'js');
   });
   afterEach(() => {
     nock.cleanAll();
   });
   it('input parameters check', () => {
-    expect(() => pageLoader()).rejects.toThrow('Invalid url format');
+    expect(() => pageLoader()).rejects.toThrow();
   });
   it('download main html', async () => {
+    const downloaded = path.join(tempdir, 'site-com-blog-about.html');
     await pageLoader('https://site.com/blog/about', tempdir);
-
-    await expect(fs.promises.access(data.html.output)).resolves.not.toThrow();
-    expect(readFile(data.html.output)).toEqual(readFile(data.html.expected));
+    await expect(fs.promises.access(downloaded)).resolves.not.toThrow();
+    expect(readFile(downloaded)).toEqual(readFile(getFixturePath('expected-site-com-blog-about.html')));
   });
-
-  it('download asset html', async () => {
+  it('download assets', async () => {
+    const assetsDir = path.join(tempdir, 'site-com-blog-about_files');
     await pageLoader('https://site.com/blog/about', tempdir);
 
-    await expect(fs.promises.access(data.htmlAsset.output)).resolves.not.toThrow();
-    expect(readFile(data.htmlAsset.output)).toEqual(readFile(data.htmlAsset.expected));
-  });
+    // js
+    const js = path.join(assetsDir, 'site-com-assets-scripts.js');
+    await expect(fs.promises.access(js)).resolves.not.toThrow();
+    await expect(readFile(js)).toEqual('js');
 
-  it('download img', async () => {
-    await pageLoader('https://site.com/blog/about', tempdir);
+    // css
+    const css = path.join(assetsDir, 'site-com-blog-about-assets-styles.css');
+    await expect(fs.promises.access(css)).resolves.not.toThrow();
+    await expect(readFile(css)).toEqual('css');
 
-    await expect(fs.promises.access(data.img.output)).resolves.not.toThrow();
-    expect(readFile(data.img.output)).toEqual(readFile(data.img.expected));
-  });
+    // img
+    const img = path.join(assetsDir, 'site-com-photos-me.jpg');
+    await expect(fs.promises.access(img)).resolves.not.toThrow();
+    await expect(readFile(img)).toEqual('img');
 
-  it('download css', async () => {
-    await pageLoader('https://site.com/blog/about', tempdir);
-
-    await expect(fs.promises.access(data.css.output)).resolves.not.toThrow();
-    expect(readFile(data.css.output)).toEqual(readFile(data.css.expected));
-  });
-
-  it('download js', async () => {
-    await pageLoader('https://site.com/blog/about', tempdir);
-
-    await expect(fs.promises.access(data.js.output)).resolves.not.toThrow();
-    expect(readFile(data.js.output)).toEqual(readFile(data.js.expected));
+    // html
+    const html = path.join(assetsDir, 'site-com-blog-about.html');
+    await expect(fs.promises.access(html)).resolves.not.toThrow();
+    await expect(readFile(html)).toEqual('html');
   });
 });
